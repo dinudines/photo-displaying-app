ce245d9e04eb8bf9c262c1b11e4ababc509ba9ca
Dinesh <dinesh25195@gmail.com>
2020-08-09 13:04:01 +0530

    add login page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from "react-router-dom";
 import HomePage from './pages/homePage';
+import LoginPage from './pages/loginPage';
 import Error404 from './pages/404';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={HomePage} />
+      <Route path='/login' component={LoginPage} />
       <Route component={Error404}/>
     </Switch>
   </BrowserRouter>
